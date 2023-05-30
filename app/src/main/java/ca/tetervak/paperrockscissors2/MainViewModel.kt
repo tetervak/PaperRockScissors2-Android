609779298469f6c872eff151760e6a5480aaa729
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-29 20:22:55 -0400

    used update() of MutableStateFlow

--- app/src/main/java/ca/tetervak/paperrockscissors2/MainViewModel.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors2/MainViewModel.kt
@@ -7,6 +7,7 @@ import ca.tetervak.paperrockscissors2.model.GameServiceImpl
 import ca.tetervak.paperrockscissors2.screens.Screen
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel() {
 
@@ -18,28 +19,28 @@ class MainViewModel : ViewModel() {
     private val gameService: GameService = GameServiceImpl()
 
     fun updateUserChoice(userChoice: Choice) {
-        val uiState = uiStateFlow.value
-        val newUiState = uiState.copy(userChoice = userChoice)
-        _uiStateFlow.value = newUiState
+        _uiStateFlow.update { uiState ->
+            uiState.copy(userChoice = userChoice)
+        }
     }
 
     fun onPlay() {
-        val uiState = uiStateFlow.value
         val computerChoice = gameService.getRandomChoice()
-        val newUiState = uiState.copy(
-            computerChoice = computerChoice,
-            gameResult = gameService.getGameResult(
-                userChoice = uiState.userChoice,
-                computerChoice = computerChoice
-            ),
-            screen = Screen.OUTPUT
-        )
-        _uiStateFlow.value = newUiState
+        _uiStateFlow.update { uiState ->
+            uiState.copy(
+                computerChoice = computerChoice,
+                gameResult = gameService.getGameResult(
+                    userChoice = uiState.userChoice,
+                    computerChoice = computerChoice
+                ),
+                screen = Screen.OUTPUT
+            )
+        }
     }
 
     fun onReplay() {
-        val uiState = uiStateFlow.value
-        val newUiState = uiState.copy(screen = Screen.INPUT)
-        _uiStateFlow.value = newUiState
+        _uiStateFlow.update { uiState ->
+            uiState.copy(screen = Screen.INPUT)
+        }
     }
 }
