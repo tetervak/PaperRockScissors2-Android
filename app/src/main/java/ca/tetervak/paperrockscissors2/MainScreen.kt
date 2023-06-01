12866d3062b9127c77a71a24f1ef3712280d5955
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-01 14:18:19 -0400

    solved the java ver issue

--- app/src/main/java/ca/tetervak/paperrockscissors2/MainScreen.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors2/MainScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+//import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,7 +24,7 @@ fun MainScreen() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            val viewModel: MainViewModel = hiltViewModel()
+            val viewModel: MainViewModel = viewModel()
             val uiState: GameUiState by viewModel.uiStateFlow.collectAsState()
 
             val navController = rememberNavController()
