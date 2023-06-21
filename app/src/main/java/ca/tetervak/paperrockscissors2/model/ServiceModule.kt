6c50d81b53375315a6d6d45e593dcc5e837f748a
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-21 13:21:00 -0400

    deleted unused imports

--- app/src/main/java/ca/tetervak/paperrockscissors2/model/ServiceModule.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors2/model/ServiceModule.kt
@@ -2,10 +2,8 @@ package ca.tetervak.paperrockscissors2.model
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
