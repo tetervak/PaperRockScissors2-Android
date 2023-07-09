0e10dad84ab67efcd9075e2a1cf4e1da8ea1f7b4
Alex Tetervak <tetervak@sheridancollege.ca>
2023-07-09 10:05:54 -0400

    adjusted button icon size

--- app/src/main/java/ca/tetervak/paperrockscissors2/ui/common/GameButton.kt
+++ app/src/main/java/ca/tetervak/paperrockscissors2/ui/common/GameButton.kt
@@ -22,7 +22,9 @@ fun GameButton(
 ) {
     Button(onClick = onClick, modifier = modifier) {
         Icon(
-            imageVector = imageVector, contentDescription = null
+            imageVector = imageVector,
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
         )
         Spacer(Modifier.size(ButtonDefaults.IconSpacing))
         Text(
